d20f6bf46856e5b0530a6cbeabf319ac13cc1419
CArtz <cartzdev@gmail.com>
2026-09-15 22:08:55 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -3,10 +3,10 @@ import bpy, os
 def register():
     folder = os.path.dirname(__file__)
     libs = bpy.context.preferences.filepaths.asset_libraries
-    if "Glitch VFX" not in libs:
-        libs.new(name="Glitch VFX", directory=folder)
+    if "CArtz VFX Effects" not in libs:
+        libs.new(name="CArtz VFX Effects", directory=folder)
 
 def unregister():
     libs = bpy.context.preferences.filepaths.asset_libraries
-    if "Glitch VFX" in libs:
-        libs.remove(libs["Glitch VFX"])
+    if "CArtz VFX Effects" in libs:
+        libs.remove(libs["CArtz VFX Effects"])
